0b0e534c7fe2c702acab232cb092b7a9394ee973
Yasnovsky <ayasnovskiy@tourmalinecore.com>
2021-06-29 05:38:03 -0700

    chore(task1): fix test and add function

--- task1/task1.js
+++ task1/task1.js
@@ -1,12 +1,21 @@
 function checkUniqueness(str) {
-for (let i = 0; i < str.length; i++) {
-  for (let j = i + 1; j < str.length; j++) {
-    if (str[i] == str[j]) {
-      return false;
+
+  if (str === '' || str === undefined || str === null) {
+
+    return false;
+
+  } else {
+      for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+          if (str[i] == str[j]) {
+            return false;
+        }
+      }
     }
+
+
+      return true;
   }
-}
-return true;
 }
 
 module.exports = checkUniqueness;
--- task1/task1.test.js
+++ task1/task1.test.js
@@ -2,6 +2,39 @@ const test = require('ava');
 
 const taskFunc = require('./task1');
 
-test('foo', t => {
-	t.notThrows(() => {taskFunc, true});
+test('a', t => {
+	t.true(taskFunc('abc'));
+});
+
+
+test('b', t => {
+	t.false(taskFunc(null));
+});
+
+
+test('c', t => {
+	t.false(taskFunc(undefined));
+
+});
+
+test('d', t => {
+	t.false(taskFunc('abbc'));
+});
+
+test('e', t => {
+	t.false(taskFunc('aabc'));
+
+});
+
+test('f', t => {
+	t.false(taskFunc('abcc'));
+
+});
+
+test('g', t => {
+	t.false(taskFunc('abcqwertyc'));
+});
+
+test('h', t => {
+	t.true(taskFunc('abcrtyui'));
 });
