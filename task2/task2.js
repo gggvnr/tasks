dd82e2c8417ee1cafa49b0a834bed46d715960b1
Yasnovsky <ayasnovskiy@tourmalinecore.com>
2021-06-30 01:22:41 -0700

    chore(second task): fix after review

--- task2/task2.js
+++ task2/task2.js
@@ -1,9 +1,5 @@
 function calcNumberIdenticalSymbols(str) {
-  let transformation = str.split('').sort(function (a,b){
-        return a.localeCompare(b);
-      }).join('');
-
-  return transformation.replace(/(.)\1+/g, (match, p1) => p1 + match.length);
+  return str.replace(/(.)\1+/g, (match, p1) => p1 + match.length);
 }
 
 module.exports = calcNumberIdenticalSymbols;
--- task2/task2.test.js
+++ task2/task2.test.js
@@ -2,18 +2,14 @@ const test = require('ava');
 
 const taskFunc = require('./task2');
 
-test('letters in order', t => {
+test('only small letters', t => {
 	t.is(taskFunc('aabccccdeeeeffgggggg'), 'a2bc4de4f2g6');
 });
 
-test('letters scatter', t => {
-	t.is(taskFunc('aavvbguhggtlern'), 'a2beg3hlnrtuv2');
+test('no order', t => {
+	t.is(taskFunc('fffsdfasfasfseqrw'), 'f3sdfasfasfseqrw');
 });
 
 test('only big letters', t => {
-	t.is(taskFunc('AABBRRYJLEERPL'), 'A2B2E2JL2PR3Y');
-});
-
-test('letters with different case', t => {
-	t.is(taskFunc('avbnAuiBbBBldfop'), 'aAb2B3dfilnopuv');
+	t.is(taskFunc('AABBRRYJLEERPL'), 'A2B2R2YJLE2RPL');
 });
